fcc1bac652213323d444b84bf88a51cc5e936e6b
Alexis <alexislsge@gmail.com>
2019-02-03 11:58:04 +0100

    Added comments TP 3

--- TP_050219/main.c
+++ TP_050219/main.c
@@ -2,11 +2,25 @@
 #include<stdlib.h>
 #include<math.h>
 
+/*
+
+
+
+[Séance n°3] TP Programmation C
+
+
+
+*/
+
 typedef int TPolynome[50];
 
 void saisie_polynome(TPolynome poly, int degre_max)
 {
     int coef;
+    /*
+
+    On commence au degré 1 et pas 0 (comme écrit dans le sujet) pour pourvoir saisir directement la constante a la fin du polynôme
+    */
     int degre = 1;
 
     do
@@ -27,7 +41,7 @@ void afficher_polynome(TPolynome poly, int degre_max)
     for(int i = degre_max; i >= 1; i--)
     {
         int coef = poly[i];
-        if(i == 1) printf("%d\n", coef);
+        if(i == 1) printf("%d\n", coef); //Pour éviter l'apparition d'un "+" après la constante à la fin du polynôme
         else printf("%dX%d + ", coef, i);   
     }
 }
@@ -46,13 +60,21 @@ int calculer_polynome(TPolynome poly, int degre_max, float x)
     return result;
 }
 
+/*
+
+Test des fonctions
+
+*/
 int main()
 {
     TPolynome poly;
 
-    saisie_polynome(poly, 4);
-    afficher_polynome(poly, 4);
+    saisie_polynome(poly, 4); //Pour cet exemple on créera un polynôme de degré 4
+    afficher_polynome(poly, 4); //On l'affiche pour voir s'il n'y a pas d'erreur dans la saisie
 
+    /*
+    Calcule d'un X saisi par l'utilisateur
+    */
     float x;
     printf("Entrez la valeur de X a calculer : ");
     scanf("%f", &x);
