66db57b9f435bbf71494b7a44fb0056d3e87e912
Alexis <alexislsge@gmail.com>
2019-04-01 22:07:22 +0200

    TP 26/03

--- TP_260319/main.c
+++ TP_260319/main.c
@@ -1,23 +1,29 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-static int numero = 0;
-
-typedef struct {
+typedef struct
+{
     char titre[50];
     char auteur[50];
     char genre[50];
     int duree;
 } TMorceau;
 
-typedef struct {
+typedef struct
+{
     int numero;
     char support[50];
     char titre_album[50];
     int nb_morceaux;
     TMorceau morceaux[20];
 } TDisque;
+
+typedef struct
+{
+    int nombreDisques;
+    TDisque disques[50];
+} TDiscotheque;
 
 TMorceau saisirMorceau()
 {
@@ -33,7 +39,7 @@ TMorceau saisirMorceau()
     scanf("%s", &morceau.genre);
 
     printf("Duree : ");
-    scanf("%d", morceau.duree);
+    scanf("%d", &morceau.duree);
 
     return morceau;
 }
@@ -41,25 +47,39 @@ TMorceau saisirMorceau()
 TDisque saisirDisque()
 {
     TDisque disque;
-    disque.numero = ++numero;
 
     printf("Support : ");
     scanf("%s", &disque.support);
 
-
     printf("Titre album : ");
     scanf("%s", &disque.titre_album);
-
 
     printf("Nombre de morceaux a saisir : ");
     scanf("%d", &disque.nb_morceaux);
 
-    for(int i = 0; i < disque.nb_morceaux; i++)
+    for (int i = 0; i < disque.nb_morceaux; i++)
     {
         disque.morceaux[i] = saisirMorceau();
     }
 
     return disque;
+}
+
+TDiscotheque saisirDiscotheque()
+{
+    TDiscotheque discotheque;
+
+    printf("Nombre de disques a saisir : ");
+    scanf("%d", &discotheque.nombreDisques);
+
+    for (int i = 0; i < discotheque.nombreDisques; i++)
+    {
+        TDisque disque = saisirDisque();
+        disque.numero = i + 1;
+        discotheque.disques[i] = disque;
+    }
+
+    return discotheque;
 }
 
 void afficherInformationsMorceau(TMorceau morceau)
@@ -69,22 +89,69 @@ void afficherInformationsMorceau(TMorceau morceau)
 
 void afficherInformationsDisque(TDisque disque)
 {
-    printf("===========[Disque %d]===========\n", disque.numero);
+    printf("\n--------------[Disque %d]--------------\n", disque.numero);
     printf("Titre album : %s\n", disque.titre_album);
     printf("Morceaux :\n");
 
-    for(int i = 0; i < disque.nb_morceaux; i++)
+    for (int i = 0; i < disque.nb_morceaux; i++)
     {
         afficherInformationsMorceau(disque.morceaux[i]);
     }
-    printf("=================================\n");
+    printf("\n------------------------------------------\n");
+}
 
+void afficherInformationsDiscotheque(TDiscotheque discotheque)
+{
+    for (int i = 0; i < discotheque.nombreDisques; i++)
+    {
+        afficherInformationsDisque(discotheque.disques[i]);
+    }
+}
+
+int dureeTotaleDisque(TDisque disque)
+{
+    int dureeTotale = 0;
+
+    for (int i = 0; i < disque.nb_morceaux; i++)
+    {
+        dureeTotale += disque.morceaux[i].duree;
+    }
+
+    return dureeTotale;
+}
+
+int dureeTotaleDiscotheque(TDiscotheque discotheque)
+{
+    int dureeTotale = 0;
+
+    for (int i = 0; i < discotheque.nombreDisques; i++)
+    {
+        dureeTotale += dureeTotaleDisque(discotheque.disques[i]);
+    }
+
+    return dureeTotale;
+}
+
+void afficherMorceaux(TDiscotheque discotheque, char auteur[50])
+{
+    for (int i = 0; i < discotheque.nombreDisques; i++)
+    {
+        TDisque disque = discotheque.disques[i];
+
+        for (int j = 0; j < disque.nb_morceaux; j++)
+        {
+            if (strcmp(disque.morceaux[i].auteur, auteur) == 0)
+            {
+                afficherInformationsMorceau(disque.morceaux[i]);
+            }
+        }
+    }
 }
 
 int main()
 {
-    TDisque disque = saisirDisque();
-    afficherInformationsDisque(disque);
+    TDiscotheque discotheque = saisirDiscotheque();
+    afficherInformationsDiscotheque(discotheque);
 
     return 0;
 }
